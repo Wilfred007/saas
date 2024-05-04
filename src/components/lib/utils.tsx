cb30f84a6b3e0cb920f840eceb6442cfb1a60899
Wilfred007 <adzerwilfred007@gmail.com>
2024-05-04 02:36:39 -0700

    class

--- src/components/lib/utils.tsx
+++ src/components/lib/utils.tsx
@@ -1,7 +1,7 @@
-import clsx from "clsx";
+import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 
-export function cn(...inputs: classValue[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
